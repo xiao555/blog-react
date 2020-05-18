958f6700bf3fd08e7ef0f621acbc0fa35f204bc0
ruiwuzhang <ruiwuzhang@clubfactory.com>
2020-05-18 17:14:01 +0800

    mod: 弃用fa

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -27,10 +27,7 @@ export default function Sidebar () {
       </p>
       <p><small>本站已存活 { total } 天</small></p>
       <button className='border-0 px-0' onClick={() => enableDarkMode(!enabled)}>
-        {
-          enabled ? <i className='fas fa-sun'></i> : <i className='fas fa-moon'></i>
-        }
-        {/* <svg width="14" height="14" class="FontsNinjaExt-main-jss16"><path d="M11.95 2.05A6.954 6.954 0 0 0 7 0C5.13 0 3.373.728 2.05 2.05A6.952 6.952 0 0 0 0 7c0 1.87.728 3.627 2.05 4.95A6.954 6.954 0 0 0 7 14c1.87 0 3.627-.728 4.95-2.05A6.954 6.954 0 0 0 14 7c0-1.87-.728-3.627-2.05-4.95zm-1.12 1.051L3.102 10.83A5.426 5.426 0 0 1 1.534 7a5.43 5.43 0 0 1 1.601-3.865A5.43 5.43 0 0 1 7 1.535c1.444 0 2.802.556 3.83 1.566z" fill="#037D5E" fill-rule="evenodd"></path></svg> */}
+        <span className={enabled ? 'icon-sun' : 'icon-moon'}></span>
       </button>
     </div>
   )
