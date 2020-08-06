c33efdf15b2ee9148e802eeb84f5f8ba648950b0
张睿武 <ruiwuzhang@clubfactory.com>
2020-08-06 13:39:43 +0800

    mod: 图标调整

--- src/index.js
+++ src/index.js
@@ -13,3 +13,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+window.addEventListener('unhandledrejection', (e) => {
+  e.preventDefault()
+  e.promise.catch(err => console.log(err))
+})
